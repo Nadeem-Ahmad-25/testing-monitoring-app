00f35a52d66cae84a827c3f6ced034dc0715cea3
Nadeem-Ahmad-25 <yusakhan.yk@gmail.com>
2025-11-26 16:15:13 +0530

    changed the script

--- script.js
+++ script.js
@@ -2,5 +2,4 @@ console.log("Version 1 - Initial");
 const data = { version: 1, message: "original content" };
 console.log("Version 2 - Initial");
 console.log("Version 2 - Initial");
-console.log("Version 3 - Initial");
-console.log("Version 4 - Initial");
+
